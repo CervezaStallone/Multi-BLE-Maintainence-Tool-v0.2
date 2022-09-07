39d5699fcd0c4518a3c8fb75da135d1f9c31a248
Cerveza <brian@cerv>
2022-09-08 00:13:26 +0200

    Fixed graph dewpoint not updating

--- data/script.js
+++ data/script.js
@@ -101,51 +101,6 @@ var gaugeDewtemp = new LinearGauge({
   barWidth: 10,
 }).draw();
 
-// Create Humidity Gauge
-var gaugeHum = new RadialGauge({
-  renderTo: 'gauge-humidity',
-  width: 300,
-  height: 300,
-  units: "Luchtvochtigheid (%RH)",
-  minValue: 0,
-  maxValue: 100,
-  colorValueBoxRect: "#049faa",
-  colorValueBoxRectEnd: "#049faa",
-  colorValueBoxBackground: "#f1fbfc",
-  valueInt: 2,
-  majorTicks: [
-      "0",
-      "20",
-      "40",
-      "60",
-      "80",
-      "100"
-
-  ],
-  minorTicks: 4,
-  strokeTicks: true,
-  highlights: [
-      {
-          "from": 80,
-          "to": 100,
-          "color": "#03C0C1"
-      }
-  ],
-  colorPlate: "#fff",
-  borderShadowWidth: 0,
-  borders: false,
-  needleType: "line",
-  colorNeedle: "#007F80",
-  colorNeedleEnd: "#007F80",
-  needleWidth: 2,
-  needleCircleSize: 3,
-  colorNeedleCircleOuter: "#007F80",
-  needleCircleOuter: true,
-  needleCircleInner: false,
-  animationDuration: 1500,
-  animationRule: "linear"
-}).draw();
-
 // Create Absolute Humidity Gauge
 var gaugeHumAbs = new RadialGauge({
   renderTo: 'gauge-humidity-absolute',
