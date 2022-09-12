adf9302dfc1808ded370b0e8746102485f53ab1f
Cerveza <brian@cerv>
2022-09-12 09:16:54 +0200

    Fixed  calculation not updating by adding function

--- src/Multi_BLE_Maintainence_Tool_v0.1.ino.cpp
+++ src/Multi_BLE_Maintainence_Tool_v0.1.ino.cpp
@@ -1,9 +1,12 @@
-/*************
- *  2022 Brian Rodriguez
- *  Multi BLE Maintainence Tool v0.1
-*************/
+# 1 "C:\\Users\\BrianR\\AppData\\Local\\Temp\\tmp0y6tb9ub"
+#include <Arduino.h>
+# 1 "G:/Mijn Drive/01. Media/11. Development/01. Arduino/01. ESP32/Multi BLE Maintainence Tool v0.1/src/Multi_BLE_Maintainence_Tool_v0.1.ino"
 
-//Include libraries
+
+
+
+
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Arduino.h>
@@ -14,51 +17,54 @@
 #include <Arduino_JSON.h>
 #include <SimpleMeteoCalc.h>
 
-//Set Const
+
 const int oneWireBus = 4;
-const char* ssid     = "Multi Maintainence Tool v0.1";
+const char* ssid = "Multi Maintainence Tool v0.1";
 const char* password = "123456789";
 
-// Variable to store the HTTP request
+
 String header;
 
-//Set oneWire lib
+
 OneWire oneWire(oneWireBus);
 
-//Set Dallas lib
+
 DallasTemperature sensors(&oneWire);
 
-//Init Simple Meteo Calc
+
 SimpleMeteoCalc mc;
 
-// Create AsyncWebServer object on port 80
+
 AsyncWebServer server(80);
 
-// Create an Event Source on /events
+
 AsyncEventSource events("/events");
 
-// Json Variable to Hold Sensor Readings
+
 JSONVar readings;
 
-// Timer variables
+
 unsigned long lastTime = 0;
 unsigned long timerDelay = 1000;
 
-//Set var for temp readings and calc
+
 float tempReadingC = sensors.getTempCByIndex(0);
-
-
-//Collect sensor data and parse to JSON
+String getSensorReadings();
+void initSPIFFS();
+void climateCalculator();
+void setup();
+void loop();
+#line 52 "G:/Mijn Drive/01. Media/11. Development/01. Arduino/01. ESP32/Multi BLE Maintainence Tool v0.1/src/Multi_BLE_Maintainence_Tool_v0.1.ino"
 String getSensorReadings(){
   readings["temperature"] = String(sensors.getTempCByIndex(0));
   readings["humidity"] = String(mc.getHumidity());
   readings["temperaturedewpoint"] = String(mc.getDewPoint());
-  readings["humidityabsolute"] =  String(mc.getHumidityAbsolute());
+  readings["humidityabsolute"] = String(mc.getHumidityAbsolute());
   String jsonString = JSON.stringify(readings);
   return jsonString;
 }
 
-//mount the SPIFFS filesystem
+
 void initSPIFFS() {
   if (!SPIFFS.begin()) {
     Serial.println("An error has occurred while mounting SPIFFS");
@@ -74,36 +80,36 @@ void climateCalculator() {
 }
 
 void setup() {
-  //Set Serial Boudrate
+
   Serial.begin(115200);
 
-  //Init filesystem
+
   initSPIFFS();
 
-  //set sensors
+
   sensors.begin();
 
 
-  //Delay to init
+
   delay(1500);
 
-  //Init WiFi AP, remove the password parameter for open AP
-  //Return IPaddress over serial Monitor
+
+
   Serial.print("Setting AP (Access Point)…");
   WiFi.softAP(ssid, password);
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
-  //server.begin();
 
-  // Set Web Server Root URL
+
+
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/index.html", "text/html");
   });
 
   server.serveStatic("/", SPIFFS, "/");
 
-  // Request for the latest sensor readings
+
   server.on("/readings", HTTP_GET, [](AsyncWebServerRequest *request){
     String json = getSensorReadings();
     request->send(200, "application/json", json);
@@ -114,18 +120,18 @@ void setup() {
     if(client->lastId()){
       Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
     }
-    // send event with message "hello!", id current millis
-    // and set reconnect delay to 1 second
+
+
     client->send("hello!", NULL, millis(), 10000);
   });
 
-  //Add and init serverhandler events
+
   server.addHandler(&events);
-  
-  // Init and start server
+
+
   server.begin();
 
-  //set meteo and calculate 
+
   mc.setTemperature(tempReadingC);
   mc.setHumidity(40.0);
   mc.setPressure(1013);
@@ -134,7 +140,7 @@ void setup() {
 
 void loop() {
   if ((millis() - lastTime) > timerDelay) {
-    // Send Events to the client with the Sensor Readings Every 0,5 seconds
+
     events.send("ping",NULL,millis());
     events.send(getSensorReadings().c_str(),"new_readings" ,millis());
     lastTime = millis();
